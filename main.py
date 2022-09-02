777b1a4f1aea0379e0cfb1546920493dcb4c6dfc
Admin <gtretyachenko@gmail.com>
2022-09-02 09:20:36 +0300

    4

--- main.py
+++ main.py
@@ -70,8 +70,8 @@ class MySqlConnector(object):
             self.db_host = cf.get_settings('mysql', 'host')
             self.db_port = cf.get_settings('mysql', 'port')
             self.db_user = cf.get_settings('mysql', 'user')
-            self.db_pwd = cf.get_settings('mysql', 'pass')
-            self.db_name = cf.get_settings('mysql', sec)
+            self.db_pwd = cf.get_settings('mysql', 'password')
+            self.db_name = cf.get_settings('mysql', 'db_name')
             self._connect = sql.connect(host=self.db_host, port=int(self.db_port), user=self.db_user,
                                         password=self.db_pwd, charset='utf8', db=self.db_name,
                                         init_command='set names utf8')
@@ -90,27 +90,29 @@ class MySqlConnector(object):
 
 
 def start_procedure():
-    columns_compare = ['Color', 'Material']
+    columns_compare = ('SeasonCode', 'Collor', 'Material')
     df_result = pd.DataFrame()
-    # date_base = MySqlConnector()
-    # sql = f'SELECT {columns_compare} FROM dim_product'
+    date_base = MySqlConnector()
+    cols = ', '.join(columns_compare)
+    sql = f'SELECT {cols} FROM dim_products'
     param = ''
     if len(param) > 0:
-        df = pd.DataFrame({'GrID': ['123456', '321654', '789654'],
-                            'SeasonCode': ['231d', '231', '231'],
-                           'Color': ['asdasd', 'asdasd', 'asdsad'],
-                           'Material': ['asdsada', 'qweqweqw', 'dasdsada']
-                           })
-        # df = pd.DataFrame(date_base.query_all(sql, param))
+        # df = pd.DataFrame({'GrID': ['123456', '321654', '789654'],
+        #                     'SeasonCode': ['231d', '231', '231'],
+        #                    'Color': ['asdasd', 'asdasd', 'asdsad'],
+        #                    'Material': ['asdsada', 'qweqweqw', 'dasdsada']
+        #                    })
+        df = pd.DataFrame(date_base.query_all(sql, param))
     else:
-        df = pd.DataFrame({'GrID': ['123456', '321654', '789654'],
-                            'SeasonCode': ['231d', '231', '231d'],
-                           'Color': ['asdasd', 'asdasd', 'asdsad'],
-                           'Material': ['asdsada', 'qweqweqw', 'dasdasdas']
-                           })
-        # df = pd.DataFrame(date_base.query_all(sql))
+        # df = pd.DataFrame({'GrID': ['123456', '321654', '789654'],
+        #                     'SeasonCode': ['231d', '231', '231d'],
+        #                    'Color': ['asdasd', 'asdasd', 'asdsad'],
+        #                    'Material': ['asdsada', 'qweqweqw', 'dasdasdas']
+        #                    })
+        df = pd.DataFrame(date_base.query_all(sql), columns=columns_compare)
+
     df_goods_compare = df[df['SeasonCode'] == '231d']
-    df_result['GrID', 'SeasonsCode', 'BaseGrID']  = [df_goods_compare['GrID'], df_goods_compare['SeasonCode'], df_goods_compare['GrID']]
+    df_result['GrID', 'SeasonCode', 'BaseGrID']  = [df_goods_compare['GrID'], df_goods_compare['SeasonCode'], df_goods_compare['GrID']]
     for col in columns_compare:
         df_goods_compare.apply(get_ratio, axis=1, str=col)
         df['str'] = str
