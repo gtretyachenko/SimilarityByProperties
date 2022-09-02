88e35ac58aa2b649d91097da3e36c903115c998b
Admin <gtretyachenko@gmail.com>
2022-09-02 08:29:51 +0300

    3

--- main.py
+++ main.py
@@ -12,19 +12,23 @@ from fuzzywuzzy import process
 
 class ConfigConnector(object):
 
-    def __ini__(self):
-        self.path_to_me = 'config.ini'
+    def __init__(self):
+        self.path_to_me = os.path.abspath(os.curdir) + r'\config.ini'
         self._connect = cnf.ConfigParser()
-        if not os.path.exists(self.path_to_me):
+        if os.path.exists(self.path_to_me):
             self._connect.read(self.path_to_me)
-            print(self)
+            self.__str__()
 
     def __str__(self):
         print(self.read_settings())
 
     def read_settings(self):
+        res = {}
         for page in self._connect.sections():
-            return {head: self._connect.get(page, head) for head in self._connect.options(page)}
+            res[page] = {}
+            for head in self._connect.options(page):
+                res[page][head] = self._connect.get(page, head)
+        return res
 
     def get_settings(self, page, header):
         return self._connect.get(page, header)
@@ -63,11 +67,11 @@ class MySqlConnector(object):
         cf = ConfigConnector()
         if not env:
             sec = 'db_{0}'.format(env)
-            self.db_host = cf.get_settings('MySql', 'host')
-            self.db_port = cf.get_settings('MySql', 'port')
-            self.db_user = cf.get_settings('MySql', 'user')
-            self.db_pwd = cf.get_settings('MySql', 'pass')
-            self.db_name = cf.get_settings('MySql', sec)
+            self.db_host = cf.get_settings('mysql', 'host')
+            self.db_port = cf.get_settings('mysql', 'port')
+            self.db_user = cf.get_settings('mysql', 'user')
+            self.db_pwd = cf.get_settings('mysql', 'pass')
+            self.db_name = cf.get_settings('mysql', sec)
             self._connect = sql.connect(host=self.db_host, port=int(self.db_port), user=self.db_user,
                                         password=self.db_pwd, charset='utf8', db=self.db_name,
                                         init_command='set names utf8')
@@ -85,24 +89,39 @@ class MySqlConnector(object):
         return result
 
 
-if __name__ == "__main__":
+def start_procedure():
+    columns_compare = ['Color', 'Material']
+    df_result = pd.DataFrame()
     # date_base = MySqlConnector()
-    # sql = ''
-    # param = ''
-    # df = pd.DataFrame(date_base.query_all(sql, param))
+    # sql = f'SELECT {columns_compare} FROM dim_product'
+    param = ''
+    if len(param) > 0:
+        df = pd.DataFrame({'GrID': ['123456', '321654', '789654'],
+                            'SeasonCode': ['231d', '231', '231'],
+                           'Color': ['asdasd', 'asdasd', 'asdsad'],
+                           'Material': ['asdsada', 'qweqweqw', 'dasdsada']
+                           })
+        # df = pd.DataFrame(date_base.query_all(sql, param))
+    else:
+        df = pd.DataFrame({'GrID': ['123456', '321654', '789654'],
+                            'SeasonCode': ['231d', '231', '231d'],
+                           'Color': ['asdasd', 'asdasd', 'asdsad'],
+                           'Material': ['asdsada', 'qweqweqw', 'dasdasdas']
+                           })
+        # df = pd.DataFrame(date_base.query_all(sql))
+    df_goods_compare = df[df['SeasonCode'] == '231d']
+    df_result['GrID', 'SeasonsCode', 'BaseGrID']  = [df_goods_compare['GrID'], df_goods_compare['SeasonCode'], df_goods_compare['GrID']]
+    for col in columns_compare:
+        df_goods_compare.apply(get_ratio, axis=1, str=col)
+        df['str'] = str
+        print(df[df['ratio'] > 10])
 
-    row = ['1', '2', '3', '4']
-    sales = {'Proper': ['erfwefw', 'erfwefw', 'asdasdasd', 'asdasdsad'],
-            }
-    df = pd.DataFrame(data=sales, index=row)
 
-    def get_ratio(row, str):
-        name = row['Proper']
-        return fuzz.token_sort_ratio(name, str)
-    str = 'erfw'
+def get_ratio(row, str):
+    name = row['Proper']
+    return fuzz.token_sort_ratio(name, str)
 
-    s = []
 
-    df.insert(1, 'ratio', df.apply(get_ratio, axis=1, str=str))
-    df.insert(2, 'str', str)
-    print(df[df['ratio'] > 10])
+if __name__ == "__main__":
+    start_procedure()
+
